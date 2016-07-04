caf68e8436354e9f9d52685e924fa36f06a87953
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-07-04 12:39:55 +0100

    import initial modules

---
+++ src/server/lib/query.js
@@ -0,0 +1,40 @@
+import Parse from 'parse/node';
+import AuthenticationFactory from './model';
+
+export function AuthenticatedQuery(sessionToken, user) {
+  // This sets up an Authenticated version
+  // of Parse.Query based on sessionToken
+  const buildOptions = (options) => {
+    const sessionData = sessionToken ? { sessionToken } : {};
+    return Object.assign(options || {}, sessionData);
+  };
+
+  class Query extends Parse.Query {
+    constructor(objectClass) {
+      super(new AuthenticationFactory(sessionToken, user).create(objectClass));
+      this.ObjectClass = new AuthenticationFactory(sessionToken, user).create(objectClass);
+    }
+
+    create(attributes = {}) {
+      return new this.ObjectClass(attributes);
+    }
+
+    count(options) {
+      return super.count(buildOptions(options));
+    }
+
+    find(options) {
+      return super.find(buildOptions(options));
+    }
+
+    first(options) {
+      return super.first(buildOptions(options));
+    }
+
+    get(objectId, options) {
+      return super.get(objectId, buildOptions(options));
+    }
+  }
+
+  return Query;
+}
